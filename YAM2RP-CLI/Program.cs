83db2a69c5e0ffea995be7e5cf1b315db1adcf4b
DodoBirb <95009974+DodoBirby@users.noreply.github.com>
2025-02-25 18:03:21 +1100

    Start moving to utmtlib

---
+++ YAM2RP-CLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using UndertaleModLib;
+
+class Program
+{
+	// Expected syntax: yam2rp.exe data.win yam2rp_project_path
+	public static void Main(string[] args)
+	{
+		if (args.Length != 2)
+		{
+			throw new Exception("Expected 2 arguments");
+		}
+		var undertaleDataPath = args[0];
+		var yam2rpPath = args[1];
+		using (var fileStream = File.OpenRead(undertaleDataPath))
+		{
+			var data = UndertaleIO.Read(fileStream);
+		}
+	}
+}
